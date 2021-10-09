f3e496cd725432c67aa988f6b30451efb7124a3f
rootxls <45496261+rootxls@users.noreply.github.com>
2021-10-09 20:34:01 +0200

    :sparkles: Add template hide template to player feature

--- src/main/java/dev/fls/tablist/tab/TabListTemplate.java
+++ src/main/java/dev/fls/tablist/tab/TabListTemplate.java
@@ -6,14 +6,16 @@ import dev.fls.tablist.tab.page.parts.Header;
 import dev.fls.tablist.tab.page.parts.body.Body;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class TabListTemplate {
 
     private final Header header = new Header();
     private final Footer footer = new Footer();
     private final Body body = new Body(this);
     private final List<UUID> displayedTo = new ArrayList<>();
-
-
 
     public Header getHeader() {
         return header;
@@ -31,13 +33,15 @@ public class TabListTemplate {
         return displayedTo;
     }
 
-    public static JavaPlugin getPlugin() {
-        return plugin;
-    }
-
     public void show(Player player) {
         displayedTo.add(player.getUniqueId());
         OptionalPart.sendPacket(player, header, footer);
-        body.display(player);
+        body.show(player);
+    }
+
+    public void hide(Player player) {
+        if(displayedTo.contains(player.getUniqueId())) displayedTo.remove(player.getUniqueId());
+        OptionalPart.sendPacket(player, new Header(), new Footer());
+        body.reset(player);
     }
 }
